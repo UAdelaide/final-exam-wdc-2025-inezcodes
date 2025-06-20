538368928fdb3d262aae30a16622b067726da363
inezcodes <a1900206@adelaide.edu.au>
2025-06-20 01:57:47 +0000

    [automatic commit] 2025-06-20 01:57:47 UTC 202.80.151.74

--- part1/Node.js/app.js
+++ part1/Node.js/app.js
@@ -22,6 +22,16 @@ function seedDatabase() {
     const insertUsers =`
         INSERT IGNORE INTO Users (username, email, password_hash, role )
         VALUES
-        ()
+        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+
+        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+
+        ('davidwalker', 'david@example.com', 'hashed321', 'walker'),
+
+        ('emilyowner', 'emily@example.com', 'hashed654', 'owner');
+
+
         `;
 };
