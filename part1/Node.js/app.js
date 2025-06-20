367c3c87fe9600da502bdb22a1bc8472ead417b6
inezcodes <a1900206@adelaide.edu.au>
2025-06-20 02:17:33 +0000

    [automatic commit] 2025-06-20 02:17:33 UTC 202.80.151.74

--- part1/Node.js/app.js
+++ part1/Node.js/app.js
@@ -87,19 +87,21 @@ app.get('/api/dogs', async (req, res) => {
 
   // /api/walkrequests/open
   app.get('/api/walkrequests/open', async (req, res) => {
-    try{
-        const query = `
+    try {
+      const query = `
         SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
         FROM WalkRequests wr
         JOIN Dogs d ON wr.dog_id = d.dog_id
         JOIN Users u ON d.owner_id = u.user_id
         WHERE wr.status = 'open';
-        `;
-        db.query(query, (err, results) => {
-            if)err) throw err;
-
-        });
-    };catch (error) {
-        res.status(500)
+      `;
+      db.query(query, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+      });
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to fetch open walk requests' });
     }
   });
+
+  /
